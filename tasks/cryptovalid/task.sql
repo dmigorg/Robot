12847623ac543da29e9da7101e8c1fbf10963661
Горгачев Дмитрий <dmigorg@gmail.com>
2022-02-13 00:20:36 +0300

    Обновление скрипта задачи

--- tasks/cryptovalid/task.sql
+++ tasks/cryptovalid/task.sql
@@ -9,7 +9,13 @@ JOIN "RequestDetail" rd ON rd."ID" = req."RequestDetailID"
 JOIN "Person" p ON p."PersonID" = COALESCE(req."RepresentativeID", req."RequesterID") 
 LEFT JOIN (
    SELECT *
-   FROM dblink('dbname=Remd user=mse_user password=123mse123','SELECT id, cryptovalid FROM remd')
+   FROM dblink(
+       'host=localhost port=5432 dbname=Remd user=mse_user password=123mse123',
+        'SELECT id, cryptovalid 
+        FROM remd
+        WHERE document_id IS NOT NULL 
+        AND export_time2 BETWEEN current_date - INTERVAL ''1 days'' AND current_date + INTERVAL ''1 day''
+   ')
    AS remd(id uuid, cryptovalid bool)
 ) AS remd ON remd.id = req."GCode"::uuid
 LEFT JOIN "DicOrganization" org ON org."ORGANIZATION_ID" = doc."DocFlowStageId" AND doc."DocFlowStageId" != 1
