5051d12138b883de8a4583b1c91b3ff4f46dbc55
dmigorg <dmigorg@gmail.com>
2022-03-31 09:32:53 +0300

    fix sql task copycertmissing

--- tasks/copycertmissing/task.sql
+++ tasks/copycertmissing/task.sql
@@ -1,4 +1,4 @@
-SELECT -- exam."Id",
+SELECT --exam."Id",
   org."SHORTNAME", p."SNILS", to_char(concl."DecisionDate",'dd.mm.yyyy') AS "DecisionDate"
 FROM "Examination" exam
 JOIN "ExaminationConclusion" concl ON concl."ExaminationId"  = exam."Id"
@@ -10,6 +10,15 @@ LEFT JOIN LATERAL (
     WHERE expdoc."ExaminationId" = exam."Id" 
 ) AS expdoc(id) ON TRUE
 WHERE exam."StateId" = 2
-    AND concl."DecisionDate" BETWEEN current_date - INTERVAL '1 days' AND current_date + INTERVAL '1 day'
-    AND expdoc.id && '{5, 7, 38, 39, 45, 46, 40, 41}'
-    AND NOT(expdoc.id && '{8}')
+  AND concl."DecisionDate" BETWEEN current_date - INTERVAL '1 days' AND current_date + INTERVAL '1 day'
+  -- есть выписка/справка
+  AND expdoc.id && '{5, 7, 38, 39, 45, 46, 40, 41}'
+  -- нет сканированной копии справки
+  AND NOT (expdoc.id && '{8}')
+  -- нет прикрепленной скан-копии
+  AND NOT EXISTS (
+    SELECT 1 
+    FROM "ExaminationExpDoc" eed 
+    JOIN "ExaminationExpDocFiles" eedf ON eedf."ExaminationExpDocId" = eed."Id" 
+    WHERE eed."ExpDocTypeId"= 8 AND eed."ExaminationId" = exam."Id"
+  )
