38039a03a9a3065723db6a4cbefb2b689a6ad957
dmigorg <dmigorg@gmail.com>
2022-08-31 16:30:12 +0300

    fix errror

--- app/config/constants.php
+++ app/config/constants.php
@@ -1,5 +1,5 @@
 <?php
 
-define('BASE_PATH', dirname(__DIR__));
-define('APP_PATH', BASE_PATH . '/app');
-define('TASK_PATH', BASE_PATH . '/tasks');
+define('BASE_PATH', __DIR__);
+define('APP_PATH', BASE_PATH . DIRECTORY_SEPARATOR . 'app');
+define('TASK_PATH', BASE_PATH . DIRECTORY_SEPARATOR . 'tasks');
