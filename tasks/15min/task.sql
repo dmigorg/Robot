cc4c9139277b7216ecfb3d1ebc8c5fbf90c7f5e1
dmigorg <dmigorg@gmail.com>
2022-08-31 13:05:58 +0300

    fiix sql 15min

--- tasks/15min/task.sql
+++ tasks/15min/task.sql
@@ -3,35 +3,44 @@ WITH rec AS (
     -- exam."Id",
     org."SHORTNAME",
     p."SNILS",
-    ( SELECT doc."Number"
+    (
+      SELECT doc."Number"
       FROM "ExaminationExpDoc" doc
       WHERE doc."ExaminationId" = exam."Id" AND (doc."ExpDocTypeId" = 10 OR doc."ExpDocTypeId" = (10 + g."Id" * 100))
-    LIMIT 1) AS "ProtocolNumber",
-    COALESCE((date_part('hour', points."StartedDateTime")*60 +date_part('minute', points."StartedDateTime")) 
-  - (date_part('hour', points."ArrivedDateTime")*60 + date_part('minute', points."ArrivedDateTime")), 0) AS delta
+      LIMIT 1
+    ) AS "ProtocolNumber",
+    (date_part('hour', points."StartedDateTime")*60 + date_part('minute', points."StartedDateTime")) - 
+    (date_part('hour', points."ArrivedDateTime")*60 + date_part('minute', points."ArrivedDateTime")) AS delta
   FROM "Examination" AS exam
   JOIN "Person" p ON p."PersonID" = exam."PatientPersonId"
   JOIN "ExaminationPointsOfOrderMse" points ON exam."Id" = points."ExaminationID"
   JOIN "DicOrganization" org ON org."ORGANIZATION_ID" = exam."ExamBuroId"
   LEFT JOIN "DicPurposeGroup" g ON (g."Id" & exam."PurposeGroupId") = g."Id"
-  WHERE exam."IsStarted" = TRUE 
-    AND (points."IsInPresence" IS NULL OR points."IsInPresence" = TRUE)
+  WHERE
+    -- МСЭ начато
+    exam."IsStarted" = TRUE
+    -- Очно
+    AND points."IsInPresence" = TRUE
+    -- На базе
     AND points."ExaminationPlaceID" = 1
-    AND (NOT EXISTS(
-      SELECT 1 FROM "ExaminationPurpose" t1 
-      WHERE t1."ExaminationPurposeID" IN (19) AND t1."ExaminationID" = exam."Id"))
+    -- Не на изменение
+    AND exam."ChangeRequestId" IS NULL
+    -- Бюро и составы (обжалование)
+    AND (points."ExamPointsOfOrderMseID" = 1 OR points."ExamPointsOfOrderMseID" IS NULL)
+    -- есть запись и явился
     AND EXISTS (
       SELECT 1 
       FROM "ExaminationRecord" r
       LEFT JOIN "ExaminationRecordExamination" er ON er."ExaminationRecordId" = r."Id"
       WHERE er."ExaminationId" = exam."Id" AND r."WasAppeared" = TRUE
-    )
-    AND exam."ExamTime" BETWEEN current_date - INTERVAL '1 days' AND current_date + INTERVAL '1 day'
+    )  
+   AND exam."ExamTime" BETWEEN current_date - INTERVAL '1 days' AND current_date + INTERVAL '1 day'
+  ORDER BY org."Number"
 )
 SELECT
   "SHORTNAME",
   "SNILS",
   "ProtocolNumber",
-  delta AS "minute"
+  COALESCE(delta::text, '-') AS "minute"
 FROM rec
-WHERE delta NOT BETWEEN 0 AND 15
+WHERE (delta NOT BETWEEN 0 AND 15 ) OR delta IS NULL
