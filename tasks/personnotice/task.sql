8bcae3672888af90871da6901dcde50c3f5bf862
dmigorg <dmigorg@gmail.com>
2022-09-13 13:23:56 +0300

    fix sql personnotice

--- tasks/personnotice/task.sql
+++ tasks/personnotice/task.sql
@@ -52,7 +52,7 @@ WITH rec AS (
       WHEN "IsRequest" AND NOT(pn."NoticeTypeIds" && '{3}') THEN 'Отсутствует "Уведомление о регистрации заявления об обжаловании..."'
       WHEN "RecordExist"  AND ("NullExamRecordsType4" > 0 OR NOT(pn."NoticeTypeIds" && '{4}')) THEN 'Отсутствует "Уведомление о проведении МСЭ (Приглашение)"'
       WHEN "RecordExist" AND ("NullExamRecordsType5" > 0 OR NOT(pn."NoticeTypeIds" && '{5}')) THEN 'Отсутствует "Уведомление о проведении МСЭ (Уведомление о дате и времени проведения МСЭ)"'
-      WHEN "PdoExist" AND NOT(pn."NoticeTypeIds" && '{6}') THEN 'Отсутствует "Уведомление о ПДО (Назначение ПДО)"'
+      WHEN "PdoExist" AND ("NullExaminationSourceId" > 0 OR NOT(pn."NoticeTypeIds" && '{6}')) THEN 'Отсутствует "Уведомление о ПДО (Назначение ПДО)"'
       WHEN "IsInPresence" = FALSE AND "PdoExist" AND NOT(pn."NoticeTypeIds" && '{7}') THEN 'Отсутствует "Уведомление о ПДО (Ответ о назначении ПДО)"'
     ELSE NULL
     END AS message,
@@ -62,11 +62,12 @@ WITH rec AS (
   LEFT JOIN LATERAL (
     SELECT array_agg(pn."NoticeTypeId"),
       SUM(1) FILTER (WHERE pn."NoticeTypeId" = 4 AND pn."ExamRecordSourceId" IS NULL),
-      SUM(1) FILTER (WHERE pn."NoticeTypeId" = 5 AND pn."ExamRecordSourceId" IS NULL)
+      SUM(1) FILTER (WHERE pn."NoticeTypeId" = 5 AND pn."ExamRecordSourceId" IS NULL),
+      SUM(1) FILTER (WHERE pn."NoticeTypeId" = 6 AND pn."ExaminationSourceId" IS NULL)
     FROM "PersonNotice" pn
     JOIN "Person" p2 ON p2."PersonID" = pn."RecipientPersonId"
     WHERE p2."RegistryPersonId" = rec."RegistryPersonId"
-  ) AS pn("NoticeTypeIds", "NullExamRecordsType4", "NullExamRecordsType5") ON TRUE
+  ) AS pn("NoticeTypeIds", "NullExamRecordsType4", "NullExamRecordsType5", "NullExaminationSourceId") ON TRUE
 )
 
 SELECT * FROM msg WHERE message IS NOT NULL
