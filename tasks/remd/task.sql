0b206343bdce213c550f9ab313698602f5439725
dmigorg <dmigorg@gmail.com>
2022-11-14 09:27:58 +0300

    add number for remd task

--- tasks/remd/task.sql
+++ tasks/remd/task.sql
@@ -1,4 +1,5 @@
 SELECT
+    ROW_NUMBER () OVER (ORDER BY rlv."StageId") AS num,
     CASE rlv."StageId" WHEN 1 THEN 'Единая регистратура' ELSE rlv."Stage" END AS stage,
     to_char(rlv."CreateTime", 'dd.mm.yyyy hh24:mi') AS createtime,
     COALESCE(rlv."RepresenterSnils", rlv."CorrPersonSnils") AS snils,
